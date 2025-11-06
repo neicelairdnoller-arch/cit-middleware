488bfaf853a61119c47685d6b37a3c1d683f0475
neicelairdnoller-arch <neice.lairdnoller@gmail.com>
2025-11-06 16:52:57 -0600

    Create routeone.js

---
+++ adapters/routeone.js
@@ -0,0 +1,4 @@
+export async function getRouteOneNotes(applicationId, { username, password }) {
+  // TODO: Replace stub with real login + scraping or API calls
+  return `RouteOne notes for ${applicationId} (stub)`;
+}
