7b774b220a5f4dfa03c09b41262e81edd147cf29
neicelairdnoller-arch <neice.lairdnoller@gmail.com>
2025-11-06 16:53:18 -0600

    Create cudl.js

---
+++ adapters/adapters/cudl.js
@@ -0,0 +1,4 @@
+export async function getCudlNotes(applicationId, { username, password }) {
+  // TODO: Replace stub with real scraping or API calls
+  return `CUDL notes for ${applicationId} (stub)`;
+}
